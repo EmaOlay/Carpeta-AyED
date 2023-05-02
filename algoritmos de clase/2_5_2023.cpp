1dd782a321b21b3f55a17373e4d8d83bd4ca7897
EmaOlay <olayemanuel@gmail.com>
2023-05-02 20:09:06 -0300

    primer ejercicio

--- algoritmos de clase/2_5_2023.cpp
+++ algoritmos de clase/2_5_2023.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
+    //declaro variables
+    char numero[4];
+    //pido ingreso de datos
+    cout << "Ingrese el numero entre 1000 y 9999: ";
+    cin >> numero;
+    for(int i =0;i<4;i++){
+        cout<<numero[i]<<endl;
+    }
     return 0;
 }
